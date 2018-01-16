4d59b8c980386a0cecdfeddeaa5ba62d87091976
Masaya Hirose <kan-hiro@post.kek.jp>
2018-01-16 13:34:01 +0900

    Remove unused callback function for connection events

--- zbxepics/pvsupport/pvsupport.py
+++ zbxepics/pvsupport/pvsupport.py
@@ -8,11 +8,7 @@ class ValQPV(PV):
     def __init__(self, pvname=''):
         self._q = Queue()
 
-        super(ValQPV, self).__init__(pvname, callback=self._on_value_change,
-                                     connection_callback=self._on_conn_change)
-
-    def _on_conn_change(self, pvname=None, conn=None, **kw):
-        logger.debug('PV connection status changed: %s %s', pvname, repr(conn))
+        super(ValQPV, self).__init__(pvname, callback=self._on_value_change)
 
     def _on_value_change(self, pvname=None, value=None, timestamp=None, **kw):
         val = (value, timestamp)
