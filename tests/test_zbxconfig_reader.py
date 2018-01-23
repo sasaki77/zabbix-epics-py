cf4a42322884dccfc23af8000ff17ab6b7fe6c07
Masaya Hirose <kan-hiro@post.kek.jp>
2018-01-23 16:42:57 +0900

    Add unittest for ZabbixConfigReader.

---
+++ tests/test_zbxconfig_reader.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import unittest
+from zbxepics.zbxconfig import ZabbixConfigReaderJSON
+
+
+class TestZabbixConfigReader(unittest.TestCase):
+
+    def test_config_reader_json(self):
+        reader = ZabbixConfigReaderJSON("test_sender_data.json")
+        items = reader.get_items()
+
+        self.assertEqual(len(items), 4)
+
+    def test_config_reader_json_err(self):
+        with self.assertRaises(KeyError):
+            ZabbixConfigReaderJSON("test_sender_data_err.json")
+
+
+def main():
+    unittest.main(verbosity=2)
+
+
+if __name__ == "__main__":
+    main()
