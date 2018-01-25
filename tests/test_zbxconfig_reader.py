ed57d58e22ec8c2ed39a07246bec593af879d721
Masaya Hirose <kan-hiro@post.kek.jp>
2018-01-25 15:21:14 +0900

    Use single quote for strings in test_zbxconfig_reader.py

--- tests/test_zbxconfig_reader.py
+++ tests/test_zbxconfig_reader.py
@@ -7,19 +7,19 @@ from zbxepics.zbxconfig import ZabbixConfigReaderJSON
 class TestZabbixConfigReader(unittest.TestCase):
 
     def test_config_reader_json(self):
-        reader = ZabbixConfigReaderJSON("test_sender_data.json")
+        reader = ZabbixConfigReaderJSON('test_sender_data.json')
         items = reader.get_items()
 
         self.assertEqual(len(items), 4)
 
     def test_config_reader_json_err(self):
         with self.assertRaises(KeyError):
-            ZabbixConfigReaderJSON("test_sender_data_err.json")
+            ZabbixConfigReaderJSON('test_sender_data_err.json')
 
 
 def main():
     unittest.main(verbosity=2)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
