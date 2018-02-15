64b8011da4f1d97e22b92046a6348849eee116ca
Masaya Hirose <kan-hiro@post.kek.jp>
2018-02-15 12:06:21 +0900

    Use os.path.join() instead of str.join()

--- tests/test_zbxconfig_reader.py
+++ tests/test_zbxconfig_reader.py
@@ -9,16 +9,16 @@ from zbxepics.zbxconfig import ZabbixConfigReaderJSON
 class TestZabbixConfigReader(unittest.TestCase):
 
     def test_config_reader_json(self):
-        config_file = '/'.join([os.path.dirname(__file__),
-                                'test_sender_data.json'])
+        dir_path = os.path.dirname(__file__)
+        config_file = os.path.join(dir_path, 'test_sender_data.json')
         reader = ZabbixConfigReaderJSON(config_file)
         items = reader.get_items()
 
         self.assertEqual(len(items), 4)
 
     def test_config_reader_json_err(self):
-        config_file = '/'.join([os.path.dirname(__file__),
-                                'test_sender_data_err.json'])
+        dir_path = os.path.dirname(__file__)
+        config_file = os.path.join(dir_path, 'test_sender_data_err.json')
         with self.assertRaises(KeyError):
             ZabbixConfigReaderJSON(config_file)
 
