3f9657dd055e094a00f9af580df3cc72bc07786c
Masaya Hirose <kan-hiro@post.kek.jp>
2018-03-13 11:24:16 +0900

    Update packages in setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(name='zbxepics',
       description='Zabbix-EPICS for Python',
       author='Masaya Hirose',
       author_email='kan-hiro@post.kek.jp',
-      packages=['zbxepics', 'zbxepics.casender',
-                'zbxepics.logging', 'zbxepics.zbxconfig'])
+      packages=['zbxepics', 'zbxepics.casender', 'zbxepics.casender.item',
+                'zbxepics.logging', 'zbxepics.zbxconfig',
+                'zbxepics.zbxconfig.apiobjects'])
